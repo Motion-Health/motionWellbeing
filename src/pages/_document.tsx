6a7ea4f2eb9502bfa0888abb29e6da561250b8c7
Alexander Street <streetalexander13@gmail.com>
2023-11-14 10:55:51 +0000

    Adding Hotjar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,6 +21,7 @@ class MyDocument extends Document {
               href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap"
             />
           </noscript>
+
           <script
             type="text/javascript"
             async
@@ -46,6 +47,23 @@ class MyDocument extends Document {
               className="invisible hidden"
             ></iframe>
           </noscript>
+
+          <script
+            type="text/javascript"
+            async
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3735105,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              `,
+            }}
+          />
         </Head>
 
         <body>
