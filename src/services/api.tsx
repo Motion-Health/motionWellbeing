f453b6f56d80013f29f306d21dddc1d6796afd9d
Alexander Street <streetalexander13@gmail.com>
2024-03-19 22:31:30 +0000

    maiun changes

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -22,18 +22,28 @@ API.interceptors.response.use(
       (err?.response?.message === 'token expired' ||
         err?.response?.data === 'Unauthorized')
     ) {
+      console.log('Redirecting to login');
+      console.log('window.location.pathname', window.location.pathname);
+      // log all the error info
+      console.log('err', err);
+      console.log('err.response', err.response);
+      console.log('err.response.data', err.response.data);
+      console.log('err.response.message', err.response.message);
+      console.log('err.response.status', err.response.status);
+      console.log('err.response.headers', err.response.headers);
+      console.log('err.config', err.config);
+      console.log('err.request', err.request);
+      console.log('err.toJSON', err.toJSON);
+      console.log('err.stack', err.stack);
+      console.log('err.message', err.message);
+
       if (window.location.pathname !== '/iframe') {
-        console.log('Redirecting to login');
-        console.log('window.location.pathname', window.location.pathname);
-        console.log('window.location', window.location);
-        console.log(err);
-        console.log(err?.response);
-        console.log(err?.response?.data);
+        
         // The current URL path is not /iframe, redirect to login
         // console.log('Redirecting to login');
         // console.log('window.location.pathname', window.location.pathname);
         // console.log('window.location', window.location);
-        (window as Window).location = '/wellbeing/login';
+        // (window as Window).location = '/wellbeing/login';
       }
     }
 
