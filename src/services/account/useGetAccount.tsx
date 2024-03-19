bd4f0b8f1fc14349094f470a2b443ce8d4a95a08
Alexander Street <streetalexander13@gmail.com>
2024-03-19 21:41:57 +0000

    api Data

--- src/services/account/useGetAccount.tsx
+++ src/services/account/useGetAccount.tsx
@@ -2,8 +2,19 @@ import { useQuery } from 'react-query'
 import { API } from '../api'
 
 const getAccount = async () => {
+  try {
   const { data } = await API.get('/account/')
   return data.account
+  } catch (error) {
+  // If the error is a response from the server, print the message
+    if (error.response) {
+      console.error(error.response.data.message);
+    } else {
+      // Otherwise, it's an error in the request itself
+      console.error(error.message);
+    }
+    throw error;
+  }
 }
 
 export const useGetAccount = () => {
