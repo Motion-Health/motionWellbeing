c37391b0f0af2e3e36d7e55638a8ec2f74e4e4e6
Dan Stevenson <danstevensonco@gmail.com>
2025-05-14 21:02:33 +0100

    update carousel and add images

--- src/components/MotionStoryCarousel/index.tsx
+++ src/components/MotionStoryCarousel/index.tsx
@@ -15,70 +15,70 @@ const storyItems: StoryItem[] = [
     title: 'Volunteering in care homes',
     description:
       "I (Zeezy) started volunteering in care homes and LOVED it. I was amazed by the stories I'd hear and people I'd meet (both residents and staff).",
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/1. story.jpg',
   },
   {
     year: '2019',
     title: 'I met Edith',
     description:
       "Whilst volunteering and delivering activities sessions in Hallamshire Care Home I met a wonderful lady called Edith. To this day Edith is the most inspiring person I've ever met!",
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/2. Story.png',
   },
   {
     year: '2020',
     title: 'COVID-19 lockdown',
     description:
       "The pandemic meant I couldn't go into care homes any more, so we developed technology that meant we could livestream in. It was around this point that Edith told me how much she wanted to go swimming.",
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/3.story.jpg',
   },
   {
     year: '2021',
     title: 'It kept families connected',
     description:
       'The tech we developed meant that families could remain connected with their loved ones in spite of where they are in the world.',
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/4. story.jpg',
   },
   {
     year: '2022',
     title: 'We kept innovating',
     description:
       'We worked with over 3,000 care providers, residents and families to continue to develop our service; ensuring it is truly person-centred.',
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/5. story.jpg',
   },
   {
     year: '2022',
     title: 'I took Edith swimming',
     description:
       'I was finally able to take Edith swimming! Despite not swimming for decades she was like a duck to water and showed me up big time!',
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/6. Story.jpg',
   },
   {
     year: '2023',
     title: "We've delivered results",
     description:
       "We've worked in partnership with incredible care providers to deliver meaningful impact and help them to grow. We're proud of our +100 Net-Promoter-Score.",
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/7. story.png',
   },
   {
     year: '2024',
     title: 'Built a passionate team',
     description:
       "I've been lucky to build a team of driven and passionate individuals who all have first-hand experience with social care.",
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/8. Story.JPG',
   },
   {
     year: '2025',
     title: "It's only the beginning",
     description:
       "We're on a mission to support you to improve and amplify care; and it really is the very start. We truly believe it's an exciting time for social care!",
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/9. story.JPG',
   },
   {
     year: '2025',
     title: "We're still best-friends",
     description:
       'I still get to pop in and see Edith weekly. She often reminds me how much better she is at swimming than I am.',
-    image: '/assets/images/placeholder.jpg',
+    image: '/assets/images/motion-story-carousel/10. story.jpg',
   },
 ];
 
